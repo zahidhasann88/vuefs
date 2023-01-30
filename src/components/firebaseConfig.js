1ff100aeecc956d727a917b047fb099db4e97a6c
Zahid Hasan <jahidhasann67@gmail.com>
2023-01-30 12:15:53 +0600

    initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default { 
+    apiKey: "AIzaSyAwHazPVvUlEqvw8Sph9e0SNThs3UraKzY",
+    authDomain: "vuefs-20b62.firebaseapp.com",
+    projectId: "vuefs-20b62",
+    storageBucket: "vuefs-20b62.appspot.com",
+    messagingSenderId: "768306693920",
+    appId: "1:768306693920:web:e6d0b4a71c4efee4053945"
+  }
